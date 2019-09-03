b09ec7f8bc372d4d6a3c2fa789fecc92434250df
Andrew Wong <ndrwchkwng@yahoo.com>
2019-09-02 23:51:37 -0400

    Obj2Lab2

---
+++ Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main( String[] args ) {
+    String name = "Andrew Wong";
+    int age = 30;
+    String state = "New Jersey";
+
+    System.out.println("Name: " + name + "Age: " + age + "State: " + state);
+  }
+}
